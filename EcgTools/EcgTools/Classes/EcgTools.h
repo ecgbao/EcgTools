8f7c38bd5e49bcbe9fa5ca0f8d250972ce4f354e
ni <baotiao.ni@mhealth365.com>
2017-02-08 10:42:09 +0800

    创建版本 1.0.0.

---
+++ EcgTools/EcgTools/Classes/EcgTools.h
@@ -0,0 +1,13 @@
+//
+//  EcgTools.h
+//  EcgTools
+//
+//  Created by baotiao ni on 2017/2/8.
+//  Copyright © 2017年 xijian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EcgTools : NSObject
+
+@end
---
+++ EcgTools/EcgTools/Classes/EcgTools.m
@@ -0,0 +1,13 @@
+//
+//  EcgTools.m
+//  EcgTools
+//
+//  Created by baotiao ni on 2017/2/8.
+//  Copyright © 2017年 xijian. All rights reserved.
+//
+
+#import "EcgTools.h"
+
+@implementation EcgTools
+
+@end
